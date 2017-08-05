ee2fcf9fde4455c682df93e70c52888fe09104ce
golden22a <golden-boy1994@hotmail.fr>
2017-08-04 18:06:07 -0700

    date update

--- vue/annonce_terminer_expediteur/index.php
+++ vue/annonce_terminer_expediteur/index.php
@@ -92,7 +92,7 @@
             <div class="navbar-header">
                 <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                 <a href="javascript:void(0);" class="bars"></a>
-                <a class="navbar-brand" href="espace_transitaire.php"><?php echo $_SESSION['Nom']; ?></a>
+                <a class="navbar-brand" href="espace_expediteur.php"><?php echo $_SESSION['Nom']; ?></a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
