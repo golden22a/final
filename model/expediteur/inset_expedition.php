cf4586cdf2be522a7863692b0145ca70f86d758f
golden22a <golden-boy1994@hotmail.fr>
2017-08-11 07:01:43 +0100

    updated

--- model/expediteur/inset_expedition.php
+++ model/expediteur/inset_expedition.php
@@ -12,11 +12,11 @@ function insert_exp($wd,$cd,$rd,$wa,$ca,$ra ,$dated,$timed,$datea,$timea,$type,$
     $req=$bdd->prepare("INSERT INTO `annonce_expediteur`(`date`,`datea`, `wilaya_d`, `commune_d`, `rue_d`, `id_user`, `tonage`, `wilaya_a`, `commune_a`, `rue_a`, `prix`, `type_camion`) VALUES (:date,:datea,:wd,:cd,:rd,:id,:tonage,:wa,:ca,:ra,:prix,:type)");
     $req->bindParam(':wd',$wd,PDO::PARAM_STR);
     $req->bindParam(':wa',$wa,PDO::PARAM_STR); /////// problm a signaler
-     $req->bindParam(':cd',$wd,PDO::PARAM_STR);
+     $req->bindParam(':cd',$cd,PDO::PARAM_STR);
     $req->bindParam(':ca',$ca,PDO::PARAM_STR);
     $req->bindParam(':ra',$ra,PDO::PARAM_STR);
-    $req->bindParam(':rd',$ra,PDO::PARAM_STR);
-    $req->bindParam(':da te',$dated,PDO::PARAM_STR);
+    $req->bindParam(':rd',$rd,PDO::PARAM_STR);
+    $req->bindParam(':date',$dated,PDO::PARAM_STR);
     $req->bindParam(':datea',$datea,PDO::PARAM_STR);
     $req->bindParam(':type',$type,PDO::PARAM_STR);
     $req->bindParam(':tonage',$tonage,PDO::PARAM_STR);
